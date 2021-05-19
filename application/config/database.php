5f5c0f88420f88812752029ba1f4e39c7e63f1fa
alvienfa <faiz.applied@gmail.com>
2021-05-19 12:06:15 +0700

    database

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	   => getenv('DATABASE_HOSTNAME'),
-	'hostname' => getenv('DATABASE_HOSTNAME'),
-	'username' => getenv('DATABASE_USERNAME'),
-	'password' => getenv('DATABASE_PASSWORD'),
+	'dsn'	   => $_SERVER['DATABASE_HOSTNAME'],
+	'hostname' => $_SERVER['DATABASE_HOSTNAME'],
+	'username' => $_SERVER['DATABASE_USERNAME'],
+	'password' => $_SERVER['DATABASE_PASSWORD'],
 	'database' => '',
 	'dbdriver' => 'pdo',
 	'dbprefix' => '',
