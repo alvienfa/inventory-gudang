367b411fd3ff7b43ebd797b49110af25da1d5717
cacingsuper <cacingzuper@gmail.com>
2021-06-04 09:51:34 +0700

    modal foto barang done

--- application/views/admin/tabel/tabel_barangmasuk.php
+++ application/views/admin/tabel/tabel_barangmasuk.php
@@ -110,19 +110,15 @@
         </div>
         <!-- /.row -->
         <div class="modal fade" id="modal-default">
-          <div class="modal-dialog">
-            <div class="modal-content">
+          <div style="width: fit-content;" class="modal-dialog">
+            <div class="modal-content ">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
                 <h4 id="modal-transaksi" class="modal-title">FOTO BARANG</h4>
               </div>
               <div class="modal-body">
-                <img class="img-fluid" id="modal-foto" src="" alt="foto-barang">
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <img width="300" class="img-fluid" id="modal-foto" src="" alt="foto-barang">
               </div>
             </div>
             <!-- /.modal-content -->
