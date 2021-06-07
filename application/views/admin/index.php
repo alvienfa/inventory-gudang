eba06e87df57993170f7b10421b8cc52c6ac6355
adit9898 <73265117+adit9898@users.noreply.github.com>
2021-06-07 14:26:36 +0700

    edit index

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -60,23 +60,7 @@
             </div>
           </div>
           
-          <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-yellow">
-              <div class="inner">
-                <?php if (!empty($dataUser)) { ?>
-                  <h3><?= $dataUser ?></h3>
-                <?php }else { ?>
-                  <h3>0</h3>
-                <?php }?>
-                <p>Users</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="<?= base_url('admin/users') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
+          
           
           <div class="col-lg-3 col-xs-6">
             <!-- small box -->
