511fd7821536380cb7f46a86466f3c7fc203bf86
cacingsuper <cacingzuper@gmail.com>
2021-07-01 09:32:34 +0700

    Update M_barang.php

--- application/models/M_barang.php
+++ application/models/M_barang.php
@@ -41,9 +41,9 @@ class M_barang extends CI_Model
     }
 
     public function total_per_barang($id_transaksi){
-        $rusak = $this->db->query("SELECT sum(jumlah) AS total_rusak FROM tb_barang_keluar AS a WHERE a.status =3 AND a.id_transaksi={$id_transaksi}")->row()->total_rusak;
-        $service = $this->db->query("SELECT sum(jumlah) AS total_service FROM tb_barang_keluar as a WHERE a.status=2 AND a.id_transaksi={$id_transaksi} ")->row()->total_service;
-        $keluar = $this->db->query("SELECT sum(jumlah) AS total_keluar FROM tb_barang_keluar as a WHERE a.status=0 AND a.id_transaksi={$id_transaksi} ")->row()->total_keluar; 
+        $rusak = $this->db->query("SELECT sum(jumlah) AS total_rusak FROM tb_barang_keluar AS a WHERE a.status =3 AND a.id_transaksi='{$id_transaksi}'")->row()->total_rusak;
+        $service = $this->db->query("SELECT sum(jumlah) AS total_service FROM tb_barang_keluar as a WHERE a.status=2 AND a.id_transaksi='{$id_transaksi}' ")->row()->total_service;
+        $keluar = $this->db->query("SELECT sum(jumlah) AS total_keluar FROM tb_barang_keluar as a WHERE a.status=0 AND a.id_transaksi='{$id_transaksi}' ")->row()->total_keluar; 
         $data = (object) array(
             'total_keluar' => $keluar,
             'total_stok' => 0,
